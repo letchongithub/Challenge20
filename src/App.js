3632ecc8e93a6739e83ef870d250888926dafc41
Letchkov <LetchkovJean@gmail.com>
2024-02-13 01:31:31 -0500

    react portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>My React Portfolio</h1>
+      <h2>This will be used to update my projects to build a profile for myself.</h2>
+      <h3>About me:</h3>
+      <h4>Projects:</h4>
+      <h5>Contact me:</h5>
+    </div>
+  );
+}
+
+export default App;
